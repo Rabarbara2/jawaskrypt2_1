a99bbf3e7fb3ba95b95924fec6eb31da1a6fa5cf
Rabarbara2 <118020475+Rabarbara2@users.noreply.github.com>
2025-10-21 09:18:43 +0000

    ????

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
   const cw1 = document.getElementById('cw1')
   const cw2 = document.getElementById('cw2')
   const cw3 = document.getElementById('cw3')
+  const cw4 = document.getElementById('cw4')
   const answer = document.getElementById('answer')
 
   example.addEventListener("click", function() {
@@ -13,7 +14,6 @@
         answer.innerHTML = JSON.stringify(array);
       })
   })
-
   cw1.addEventListener("click", function() {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
@@ -45,7 +45,26 @@
   })
 
   cw3.addEventListener("click", function() {
-    //TODO
-  })
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+      .then(response => response.json())
+      .then(post => {
+        answer.innerHTML += `<h2>${post.title}</h2>`
+        answer.innerHTML += `<p>${post.body}</p>`
+        answer.innerHTML += `<hr>`
 
-})();
+      })
+  })
+  
+
+})
+/*  cw4.addEventListener("click", async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: { contentType: "application/json}" },
+      body: JSON.stringify({
+        title: "Łutyt",
+        body: "Kotek miau miau miau",
+        userId: 1,
+      }),
+    });
+  })*/
